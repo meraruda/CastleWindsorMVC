461b8a6d32086a2e94e4e0d153e3dfb4c4c06806
Wilson WS Huang/WHQ/Wistron <Wilson_WS_Huang@wistron.com>
2017-08-16 14:18:33 +0800

    add dynamicproxy part

---
+++ CastleWindsor/DynamicProxy.cs
@@ -0,0 +1,96 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleWindsor
+{
+    internal interface IFreezable
+    {
+        bool IsFrozen { get; }
+        void Freeze();
+    }
+
+    internal class FreezableInterceptor : IInterceptor, IFreezable
+    {
+        private bool _IsFrozen;
+
+        public void Freeze()
+        {
+            _IsFrozen = true;
+        }
+
+        public bool IsFrozen
+        {
+            get { return _IsFrozen; }
+        }        
+
+        public void Intercept(IInvocation invocation)
+        {
+            if(_IsFrozen && invocation.Method.Name.StartsWith("set_", StringComparison.OrdinalIgnoreCase))
+                throw new Exception("ObjectFrozenException");
+
+            invocation.Proceed();
+        }
+    }
+
+    public static class Freezable
+    {
+        private static readonly IDictionary<object, IFreezable> _freezables = new Dictionary<object, IFreezable>();
+        private static readonly ProxyGenerator _generator = new ProxyGenerator();
+
+        public static bool IsFreezable(object obj)
+        {
+            return obj != null && _freezables.ContainsKey(obj);
+        }
+
+        public static void Freeze(object freezable)
+        {
+            if(!IsFreezable(freezable))
+                throw new Exception("NotFreezableObjectException");
+
+            _freezables[freezable].Freeze();
+        }
+
+        public static bool IsFrozen(object freezable)
+        {
+            return IsFreezable(freezable) && _freezables[freezable].IsFrozen;
+        }
+
+        public static TFreezable MakeFreezable<TFreezable>() where TFreezable : class, new()
+        {
+            var freezableInterceptor = new FreezableInterceptor();
+            var proxy = _generator.CreateClassProxy<TFreezable>(new CallLoggingInterceptor(), freezableInterceptor);
+
+            _freezables.Add(proxy, freezableInterceptor);
+            return proxy;
+        }
+    }
+
+    internal class CallLoggingInterceptor : IInterceptor
+    {
+        public CallLoggingInterceptor()
+        {
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            invocation.Proceed();
+        }
+    }
+
+    public class Pet
+    {
+        public virtual string Name { get; set; }
+        public virtual int Age { get; set; }
+        public virtual bool Deceased { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Deceased: {Deceased}";
+        }
+    }
+
+}
---
+++ CastleWindsor/Program.cs
@@ -0,0 +1,103 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using System;
+
+namespace CastleWindsor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DynamicProxy
+
+            var rex = Freezable.MakeFreezable<Pet>();
+            rex.Name = "Rex";
+
+            Console.WriteLine(Freezable.IsFreezable(rex) ? "Rex is freezable!" : "Rex is not freezable. Something");
+            Console.WriteLine(rex.ToString());
+            Console.WriteLine("Add 50 yeadrs");
+            rex.Age += 50;
+            Console.WriteLine("Age: {0}", rex.Age);
+            rex.Deceased = true;
+
+            Console.WriteLine("Deceased: {0}", rex.Deceased);
+            Freezable.Freeze(rex);
+
+            try
+            {
+                rex.Age++;
+            }
+            catch(ObjectDisposedException ex)
+            {
+                Console.WriteLine("Oups. it's frozen. Can't change that anymore");
+            }
+
+            Console.ReadLine();
+#endif
+
+#if Ioc
+            var container = new WindsorContainer();
+
+            container.Install(FromAssembly.This());
+
+            var myObject = container.Resolve<Rowan>();
+            //var myObject = new Rowan(new Dependency1(), new Dependency2());
+
+            myObject.DoSomething();
+
+            Console.ReadKey();
+#endif
+
+        }
+    }
+
+    public class WindsorInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Classes.FromThisAssembly().Pick().WithServiceAllInterfaces());
+        }
+    }
+
+    public class Rowan
+    {
+        private IDenpendency1 object1;
+        private IDenpendency2 object2;
+
+        public Rowan(IDenpendency1 dependency1, IDenpendency2 dependency2)
+        {
+            object1 = dependency1;
+            object2 = dependency2;
+        }
+
+        public void DoSomething()
+        {
+            object1.SomeObject = "Hello 1";
+            object2.SomeOtherObject = "Hello 2";
+
+            Console.WriteLine($"{object1.SomeObject}\n{object2.SomeOtherObject}");
+        }
+    }
+
+    public interface IDenpendency2
+    {
+        object SomeOtherObject { get; set; }
+    }
+
+    public interface IDenpendency1
+    {
+        object SomeObject { get; set; }
+    }
+
+    public class Dependency1 : IDenpendency1
+    {
+        public object SomeObject { get; set; }
+    }
+
+    public class Dependency2 : IDenpendency2
+    {
+        public object SomeOtherObject { get; set; }
+    }
+}
